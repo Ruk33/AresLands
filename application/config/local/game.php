6f92b78984706d6c4dd4f08e1be610a333a10144
Ruke <area51ruke@gmail.com>
2013-07-05 16:43:33 -0300

    agregada configuracion local

---
+++ application/config/local/game.php
@@ -0,0 +1,60 @@
+<?php
+
+return array(
+	/*
+	 *	Id del objeto que hace
+	 *	de experiencia
+	 */
+	'xp_item_id' => 9998,
+
+	/*
+	 *	Id del objeto que hace de moneda
+	 *	(cobre)
+	 */
+	'coin_id' => 9999,
+
+	/*
+	 *	Cantidad de monedas (en cobre)
+	 *	que cuesta viajar
+	 */
+	'travel_cost' => 10,
+
+	/*
+	 *	Tiempo que se tarda
+	 *	en finalizar el viaje
+	 *	(en segundos)
+	 */
+	'travel_time' => 60,
+
+	/*
+	 *	Tiempo de descanzo (en segundos)
+	 *	después de una pelea
+	 */
+	'battle_rest_time' => 60,
+
+	/*
+	 *	Puntos que se otorgan
+	 *	al pasar de nivel
+	 */
+	'points_per_level' => 7,
+
+	/*
+	 *	Mínima cantidad (en segundos)
+	 *	de tiempo para explorar
+	 */
+	'min_explore_time' => 300,
+
+	/*
+	 *	Máxima cantidad (en segundos)
+	 *	de tiempo para explorar
+	 */
+	'max_explore_time' => 14400,
+
+	/*
+	 *	Rates
+	 */
+	'xp_rate' => 1,
+	'drop_rate' => 1,
+	'explore_reward_rate' => 1,
+	'coins_rate' => 1,
+);
