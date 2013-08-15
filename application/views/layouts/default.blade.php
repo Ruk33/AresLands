6133774b995e61db8d992933bbd6ff6660a55a44
Ruke <area51ruke@gmail.com>
2013-08-15 01:15:12 -0300

    default.layout ahora revisa el enviroment para cargar js (minificado o no)

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -160,13 +160,17 @@
 
 		<script src="{{ URL::base() }}/js/vendor/angular.min.js"></script>
 
-		<script src="{{ URL::base() }}/js/app.js"></script>
-		
-		<script src="{{ URL::base() }}/js/configuration.js"></script>
-		<script src="{{ URL::base() }}/js/services.js"></script>
-		<script src="{{ URL::base() }}/js/controllers.js"></script>
-		<script src="{{ URL::base() }}/js/filters.js"></script>
-		<script src="{{ URL::base() }}/js/directives.js"></script>
+		@if ( Request::env() == 'local' )
+			<script src="{{ URL::base() }}/js/app.js"></script>
+			
+			<script src="{{ URL::base() }}/js/configuration.js"></script>
+			<script src="{{ URL::base() }}/js/services.js"></script>
+			<script src="{{ URL::base() }}/js/controllers.js"></script>
+			<script src="{{ URL::base() }}/js/filters.js"></script>
+			<script src="{{ URL::base() }}/js/directives.js"></script>
+		@else
+			<script src="{{ URL::base() }}/js/app.min.js"></script>
+		@endif
 
 		<!--
 		<script src="{{ URL::base() }}/js/vendor/modernizr-2.6.2.min.js"></script>
