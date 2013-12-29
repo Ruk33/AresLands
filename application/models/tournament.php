2a0960b80e8bf91bcdd66c2e821d7397061d7ff8
Ruke <area51ruke@gmail.com>
2013-12-29 09:35:36 -0200

    usando variable inexistente en lugar de this

--- application/models/tournament.php
+++ application/models/tournament.php
@@ -340,7 +340,7 @@ class Tournament extends Base_Model
 		foreach ( $registeredClans as $registeredClan )
 		{
 			$tmpClan = $registeredClan->clan;
-			$tmpScore = TournamentClanScore::get_victory_percentage($tournament->id, $clan->id);
+			$tmpScore = TournamentClanScore::get_victory_percentage($this->id, $clan->id);
 
 			if ( $clanWinnerScore < $tmpScore )
 			{
