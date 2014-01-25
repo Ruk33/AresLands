aeda90630e3aa80b5add28c7e11970b627eb8bab
Franco Montenegro - Ruke <area51ruke@gmail.com>
2014-01-25 13:26:14 -0300

    ahora si corregido el tiempo en el chat

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -245,19 +245,19 @@ controller('CharacterController', ['$scope', '$http', '$timeout', 'CharacterOfLo
 		}
 		
 		var d = new Date(time * 1000);
-		var hours = d.getHours();
-		var minutes = d.getMinutes();
-		var seconds = d.getSeconds();
+		var hours = new String(d.getHours());
+		var minutes = new String(d.getMinutes());
+		var seconds = new String(d.getSeconds());
 		
-		if ( hours.length < 2 ) {
+		if ( hours.length != 2 ) {
 			hours = '0' + hours;
 		}
 		
-		if ( minutos.length < 2 ) {
+		if ( minutes.length != 2 ) {
 			minutes = '0' + minutes;
 		}
 		
-		if ( seconds.length < 2 ) {
+		if ( seconds.length != 2 ) {
 			seconds = '0' + seconds;
 		}
 		
