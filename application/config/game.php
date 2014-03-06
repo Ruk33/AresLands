81ee055ffda1fbcf934ab88a3a9a965646ad87c5
Ruke <area51ruke@gmail.com>
2014-03-06 20:15:13 -0200

    aumentado drasticamente rate de monedas

--- application/config/game.php
+++ application/config/game.php
@@ -195,6 +195,6 @@ return array(
 	'quest_xp_rate' => 0.8,
 	'drop_rate' => 1,
 	'explore_reward_rate' => 1,
-	'coins_rate' => 1.4,
-	'quest_coins_rate' => 2.3,
+	'coins_rate' => 3,
+	'quest_coins_rate' => 5,
 );
