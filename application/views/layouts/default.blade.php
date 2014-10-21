205d746bb8114b451d22f5244a14ecaeb638b513
Ruke <area51ruke@gmail.com>
2014-10-21 13:42:28 -0300

    actualizado footer de ironfist a titangames

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -458,19 +458,19 @@
                 <div style="background-image: url({{ URL::base() }}/img/footer-content.png); height: 100px; margin-top: -20px; background-position: 1px 0; background-repeat: no-repeat; position: relative; z-index: 2; width: 101%;"></div>
 				
 				<div id="footer">
-					<div class="pull-left">
-						<a href="//ironfist.com.ar" target="_blank">
-							<img src="{{ URL::base() }}/img/ironfist-logo.png" style="width: 106px; height: 129.5px;">
+					<div class="pull-left" style="padding: 25px;">
+						<a href="//titangames.com.ar" target="_blank">
+							<img src="{{ URL::base() }}/img/logo-titangames.png">
 						</a>
 					</div>
 					<div style="padding-top: 50px; margin-left: 130px;">
 						<p>
 							Todas las marcas aquí mencionadas son propiedad de sus respectivos dueños. 
 							<br>
-							©2014 IronFist. Todos los derechos reservados.
-							<a href="//ironfist.com.ar/privacy" target="_blank">Política de privacidad</a>
+							©2014 TitanGames. Todos los derechos reservados.
+							<a href="//titangames.com.ar/privacy" target="_blank">Política de privacidad</a>
 							-
-							<a href="//ironfist.com.ar/terms" target="_blank">Condiciones generales de uso</a>
+							<a href="//titangames.com.ar/terms" target="_blank">Condiciones generales de uso</a>
 							<br>
 							AresLands es una realidad gracias a <b>Vicente Buendia</b> y a su proyecto open <a href="http://sourceforge.net/projects/tierras/" target="_blank" rel="nofollow">Tierras de Leyenda</a> y a <a href="{{ URL::to('home/thanks') }}">todos los que contribuyeron con nosotros</a>.
 						</p>
